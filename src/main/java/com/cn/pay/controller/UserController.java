ef29c72492d4a7bc16ef4b656d6aaed420dc4835
miao10086 <miao0980980@qq.com>
2017-09-04 17:53:39 +0800

    测试条件查询成功！！！

--- src/main/java/com/cn/pay/controller/UserController.java
+++ src/main/java/com/cn/pay/controller/UserController.java
@@ -6,15 +6,16 @@ import com.cn.pay.srevice.inf.IUserService;
 import com.cn.pay.utils.AjaxResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.annotation.Resource;
+import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 /**
  * 项目名称:
@@ -30,22 +31,53 @@ public class UserController {
 
     private static Logger sLogger = LoggerFactory.getLogger(UserController.class);
 
-    @Resource
-    private IUserService iUserService;
+    @Autowired
+    private IUserService<User> iUserService;
 
     @RequestMapping(value = "/createUser", method = RequestMethod.POST)
     @ResponseBody
     public AjaxResult createUser(@RequestBody User user) {
         AjaxResult ajaxResult = null;
         try {
+            user.setCreateTime(new Date());
             iUserService.createUser(user);
             ajaxResult = new AjaxResult(true, ResultCode.SUCCESS);
+            sLogger.info(ResultCode.SUCCESS);
         } catch (Exception e) {
-            e.printStackTrace();
+            sLogger.error("createUser：{}", e);
         }
-
         return ajaxResult;
     }
 
+    @RequestMapping(value = "/queryById", method = RequestMethod.POST)
+    @ResponseBody
+    public AjaxResult getById(@RequestBody User user) {
+        AjaxResult ajaxResult = null;
+        try {
+            User userResult = iUserService.getById(user.getId());
+            ajaxResult = new AjaxResult(true, ResultCode.SUCCESS);
+            ajaxResult.setResultData(userResult);
+        } catch (Exception e) {
+            sLogger.error("queryById：{}", e);
+        }
+        return ajaxResult;
+    }
+
+    @RequestMapping(value = "getByWhere", method = RequestMethod.POST)
+    @ResponseBody
+    public AjaxResult getByWhere(@RequestBody User user) {
+        AjaxResult ajaxResult = null;
+        try {
+            HashMap<String, Object> params = new HashMap<String, Object>();
+            params.put("userName", user.getUserName());
+            params.put("id", user.getId());
+            List<User> user1 = iUserService.getByWhere(params);
+            ajaxResult = new AjaxResult(true, ResultCode.SUCCESS);
+            ajaxResult.setResultData(user1);
+        } catch (Exception e) {
+            sLogger.error("getByWhere：{}",e);
+        }
+        return ajaxResult;
+    }
 
 }
